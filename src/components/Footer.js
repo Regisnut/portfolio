ad07e715a6276e22337830be42a8db377343a072
Régis NUTTIN <regisnut@hotmail.com>
2020-10-26 00:15:54 +0100

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,20 +5,47 @@ import { Link } from "gatsby"
 const Footer = () => {
   return (
     <footer className="footer">
+
+<div className="footer__container">
+<div className="footer__left">
+  <h3>Front-End Developer</h3>
+  <h4>Web Design</h4>
+  <h4>Web Development</h4>
+  <h4>App Design</h4>
+</div>
+<div className="footer__right">
+     <Link to="/">
+       <h4 className="footer__right__link"> 
+         Home
+         </h4>
+       </Link>
+          <Link to="projects">
+            <h4 className="footer__right__link">Projects
+              </h4>
+          </Link>
+          <Link to="blog"> 
+                 <h4 className="footer__right__link">
+                   Blog
+                   </h4></Link>
+          <Link to="about">
+                   <h4 className="footer__right__link">
+                    About 
+                   </h4>
+             </Link>
+          <Link to="contact"> 
+                 <h4 className="footer__right__link">
+                   Contact
+                   </h4></Link>
+</div>
+ <SocialLinks styleClass="footer-links" />
+</div>
       <div>
-        <SocialLinks styleClass="footer-links" />
-        <p className="footer-menu">
-          <Link to="/">Home |</Link>
-          <Link to="projects"> Projects |</Link>
-          <Link to="blog"> Blog |</Link>
-          <Link to="about"> About |</Link>
-          <Link to="contact"> Contact</Link>
-        </p>
         <h4>
           copyright&copy;{new Date().getFullYear()}
           <span> website</span> all rights reserved
         </h4>
       </div>
+
     </footer>
   )
 }
