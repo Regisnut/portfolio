078faa7f7e35159ac843c75e34eb139544dadcc5
Régis NUTTIN <regisnut@hotmail.com>
2020-11-24 21:00:17 +0100

    fix commit after formik

--- src/i18n/withTrans.js
+++ src/i18n/withTrans.js
@@ -3,15 +3,15 @@ import i18next from './config';
 import { I18nextProvider, withTranslation } from 'react-i18next';
 
 export function withTrans(WrappedComponent) {
-    WrappedComponent = withTranslation()(WrappedComponent);
+  WrappedComponent = withTranslation()(WrappedComponent);
 
-    return class extends Component {
-      render() {
-        return (
-          <I18nextProvider i18n={i18next}>
-            <WrappedComponent {...this.props} language={i18next.language} />
-          </I18nextProvider>
-        );
-      }
+  return class extends Component {
+    render() {
+      return (
+        <I18nextProvider i18n={i18next}>
+          <WrappedComponent {...this.props} language={i18next.language} />
+        </I18nextProvider>
+      );
     }
+  }
 }
